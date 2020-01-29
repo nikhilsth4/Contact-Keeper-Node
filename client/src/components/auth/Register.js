f3308b0afaab5374a0c2c2b8928aaaf24334c5d1
Nikhilshresth <nikhilshrestha0@gmail.com>
2020-01-29 18:01:16 +0545

    register completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Register = () => {
+const Register = props => {
   const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
+
+  const { register, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+    }
+  }, [error, isAuthenticated, props.history]);
 
   const [user, setUser] = useState({
     name: '',
@@ -25,8 +39,13 @@ const Register = () => {
       setAlert('Please enter all fields', 'danger');
     } else if (password !== password2) {
       setAlert('Password donot match', 'danger');
+      clearErrors();
     } else {
-      console.log('REgister submit');
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
 
